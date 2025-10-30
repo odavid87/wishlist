2d9a1e2dcf49a87e06a1350d45e9e462262920c6
David Orban <orban.david.mark@gmail.com>
2025-10-30 15:00:00 +0100

    Favicon

--- app/index.php
+++ app/index.php
@@ -8,7 +8,13 @@
     <meta property="og:description" content="Oszd meg egyszerűen az ünnepi kívánságlistádat a családdal!">
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://mamahuncutkai.hu">
+    <meta property="og:image" content="/assets/favicon/android-chrome-512x512.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/favicon/site.webmanifest">
+    <link rel="shortcut icon" href="/assets/favicon/favicon.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
