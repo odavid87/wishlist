2ae851564581dd57fc1adde1d36d0379fc200782
David Orban <orban.david.mark@gmail.com>
2025-10-30 13:10:13 +0100

    Changes db file's name

--- app/includes/config.php
+++ app/includes/config.php
@@ -1,3 +1,3 @@
 <?php
-define('DATA_FILE', __DIR__ . '/../../data/whishlist.json');
+define('DATA_FILE', __DIR__ . '/../../data/wishlist.json');
 define('APP_PASSWORD', getenv('APP_PASSWORD') ?: 'changeme');
